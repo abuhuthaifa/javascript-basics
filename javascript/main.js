6dc59b16e21c0b5e5a03f3c4528d53b7e53dc1b6
reed jayne <reed jayne>
2016-03-21 16:25:54 -0700

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,94 @@
+
+//console.log("hello codify academy")
+//document.write("whats up")
+//alert("buddy")
+//var userInput= prompt("whats your name")
+//document.write(userInput)
+
+//var num= 12
+//var numtwo = "bob"
+//var tru= true
+//var str="this is string"
+//console.log(numtwo)
+
+//var numArray= [33, 17, 89, 64, 78, 22]
+//console.log(numArray[2])
+
+//var strArray = ["this", "is", "my", "string","array"]
+//console.log(strArray)
+
+//var stringValue="hello"
+//var stringValue2="World!"
+
+//var combineVariables= stringValue + stringValue2
+
+//console.log(combineVariables)
+
+//var str1="Hello"
+//var str2="codify"
+//console.log(str1 + str2)
+
+//if (str1== "hello") {
+//	alert(str1 + str2)
+//}
+//else{
+//	alert("this is not true")
+//}
+
+//var answer= prompt("choose a number between 1 and 100")
+
+//if(answer > 50){
+//	alert("your answer is higher is higher than 50")
+//}
+
+//else {
+//	alert("your answer is less than 50")
+//}
+
+//var num1= 10
+//var num2 = 15 
+//function thisFunctionsName(a, b) {
+//	alert(a + b)
+//}
+
+//thisFunctionsName()
+
+document.write("Goodbye World")
+console.log("Goodbye World")
+alert("Goodbye World")
+var userName = prompt ("Whats your name?")
+var str="reed"
+console.log(str)
+var num= 2 
+console.log(num)
+var boolean= true
+console.log(boolean)
+var mixedArray=[13,"me", true, 45]
+console.log(mixedArray)
+var stringValue1="reed"
+var stringValue2="jayne"
+var combineVariables= stringValue1 + stringValue2
+console.log(combineVariables)
+
+var numValue1=20
+var numValue2=40
+if(numValue1< numValue2){
+	console.log("Equation is true")
+}
+
+else{
+	console.log("Equation is false")
+}
+
+for(i=5; i<=24; i++) {
+	console.log(i)
+}
+
+var name1 = 15
+var name2 = 20
+
+function addNumbers(a,b) {
+	console.log (a + b)
+}
+
+addNumbers (name1, name2)
